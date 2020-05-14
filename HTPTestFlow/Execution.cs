670fa44367571714b10e7f0830e17be637575cd3
Lihua DAI <lihua.dai@ericsson.com>
2020-05-14 09:36:51 +0800

    Add project files.

---
+++ HTPTestFlow/Execution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace HTPTestFlow
+{
+    class ExecutionTPStart
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            Console.ReadLine();
+            int a = 1;
+
+            //Console.WriteLine("Select Test Mode: Auto or Manual?");
+            //string TestMode = Console.ReadLine();
+            //Console.WriteLine($"\nStart in {TestMode} Mode");
+
+            if (a == 1)
+            {
+                Console.WriteLine("Start in Auto Mode");
+                Console.ReadLine();
+                Console.WriteLine("TP_START");
+                Console.ReadLine();
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("TP_RUN_SELECT");
+                Console.ReadLine();
+                Console.WriteLine("OK");
+            }         
+        }
+    }
+}
